e2401522faec544288d1c750ebf14d1af7a0714f
豆浆不好喝 <yaolihua@doujiangbuhaohedeMacBook-Air.local>
2026-03-02 11:00:07 +0800

    Remove next-pwa and simplify next.config.js for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
